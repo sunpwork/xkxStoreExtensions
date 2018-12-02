b0b5123cfd0ff7bdc08ebfc192126b1483fac3fe
sunpwork <sunpwork@qq.com>
2018-12-02 19:48:06 +0800

    done order store

--- js/background.js
+++ js/background.js
@@ -16,19 +16,42 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     console.log(request);
     request.newOrderList.forEach(function (element, index) {
         $.get(element.url, function (result) {
-            var userInfo = $(result).find('.content-bd.table_list2').children('div')[0].innerText;
-
-            var orderInfoDivList = $(result).find('.cnt').children('.data-table').children('tbody').children('tr').children('.c');
-
-            var orderInfo = {};
-            for (var i = 0; i < orderInfoDivList.length; i+=4) {
-                orderInfo['name'] = orderInfoDivList[i].innerText;
-                orderInfo['price'] = orderInfoDivList[i+1].innerText;
-                orderInfo['count'] = orderInfoDivList[i+2].innerText;
+            var userInfoDiv = $(result).find('.content-bd.table_list2').children('div')[0];
+            var goodsDivList = $(result).find('.cnt').children('.data-table').children('tbody').children('tr').children('.c');
+            var goodsList = new Array();
+            for (var i = 0; i < goodsDivList.length; i += 4) {
+                goodsList.push({
+                    'name': goodsDivList[i].innerText,
+                    'price': goodsDivList[i + 1].innerText,
+                    'count': goodsDivList[i + 2].innerText
+                });
             }
-            orderInfo['userInfo'] = userInfo;
 
-            $.get("http://127.0.0.1/sendMsgToUser")
+            var user_info = userInfoDiv.innerText.replace(/((\s)*\n(\s)*)+/g,'\n').split('\n');
+
+            var orderInfo = {
+                'brand_id': request.brand_id,
+                'platform_id': element.id,
+                'order_time': element.order_time,
+                'goodsList': goodsList,                
+                'user_remark': user_info[0],
+                'user_name': user_info[1],
+                'user_tel': user_info[2],
+                'user_address':user_info[3] 
+            }
+
+            $.ajax({
+                url: "http://xkxshopserver.test/order/store",
+                data: JSON.stringify(orderInfo),
+                contentType: "application/json; charset=utf-8",
+                type: "POST",
+                success: function (result) {
+
+                }
+            });
+
+            console.log(orderInfo);
+
         })
     });
 });
--- js/orderList.js
+++ js/orderList.js
@@ -1,16 +1,16 @@
-
 var orderIdDivList = $('tr').children('.chk').children('input');
+var orderTimeDivList = $('tr').children('.c[nowrap="nowrap"]:even');
 var orderUrlDivList = $('tr').children('.icon').children('.opt[target="_blank"]');
-var brandId = $("#keys_b").val();
+var brand_id = $("#keys_b").val();
 
-console.log("brandId: " + brandId);
+console.log("brand_id: " + brand_id);
 
 //从storage获取最后一次发送短信的订单编号
 chrome.storage.local.get(function (items) {
     var latestIdList = items['latestIdList'];
     console.log(latestIdList);
-    if (latestIdList && latestIdList[brandId]) {
-        var latestId = latestIdList[brandId];
+    if (latestIdList && latestIdList[brand_id]) {
+        var latestId = latestIdList[brand_id];
 
         var newOrderList = Array();
 
@@ -18,22 +18,22 @@ chrome.storage.local.get(function (items) {
             // if (orderIdDivList[i].value == latestId) {
             //     break;
             // }
-            newOrderList.push({ 'id': orderIdDivList[i].value, 'url': orderUrlDivList[i].href })
+            newOrderList.push({ 'id': orderIdDivList[i].value, 'order_time': orderTimeDivList[i].innerText ,'url': orderUrlDivList[i].href })
         }
-        chrome.runtime.sendMessage({ 'brandId': brandId, 'newOrderList': newOrderList }, function (response) {
+        chrome.runtime.sendMessage({ 'brand_id': brand_id, 'newOrderList': newOrderList }, function (response) {
 
         });
 
     }
 
-    // if (latestIdList == undefined) {
-    //     latestIdList = new Array();
-    // }
-    // latestIdList[brandId] = orderIdDivList[0].value;
+    if (latestIdList == undefined) {
+        latestIdList = new Array();
+    }
+    latestIdList[brand_id] = orderIdDivList[0].value;
 
-    // chrome.storage.local.set({ 'latestIdList': latestIdList }, function () {
-    //     console.log(latestIdList);
-    // });
+    chrome.storage.local.set({ 'latestIdList': latestIdList }, function () {
+        console.log(latestIdList);
+    });
 })
 
 
